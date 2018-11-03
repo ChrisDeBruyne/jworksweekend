4515ce6e5ef0ed94e698f9d9fa0e67efcb40a711
chris382 <debruyne.chris@gmail.com>
2018-11-03 13:48:53 +0100

    next

--- src/main/java/com/example/moviedemo/MovieController.java
+++ src/main/java/com/example/moviedemo/MovieController.java
@@ -1,13 +1,18 @@
 package com.example.moviedemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("movie")
 public class MovieController {
 
+    @Autowired
+    private RestClient
+
     @GetMapping
     public String getMovie(){
+
         return "bla";
     }
 }
