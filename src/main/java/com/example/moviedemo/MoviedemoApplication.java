5e4af0cdc97f8b517dc58918e6217ecc2b99699c
chris382 <debruyne.chris@gmail.com>
2018-11-03 13:37:18 +0100

    start

---
+++ src/main/java/com/example/moviedemo/MoviedemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.moviedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviedemoApplication.class, args);
+    }
+}
