a29308e59a8ff4a5980b7e1e0631ecce0eb0fc93
chris382 <debruyne.chris@gmail.com>
2018-11-03 14:03:42 +0100

    next

--- src/main/java/com/example/moviedemo/MovieController.java
+++ src/main/java/com/example/moviedemo/MovieController.java
@@ -1,18 +1,18 @@
 package com.example.moviedemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController("movie")
 public class MovieController {
 
-    @Autowired
-    private RestClient
+    private String apiKey = "3c5dc7cf33e70ad798ec4337a7a14605";
 
     @GetMapping
-    public String getMovie(){
-
-        return "bla";
+    public String getMovie(@RequestParam String id){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("https://api.themoviedb.org/4/list/"+ id + "?page=1&api_key=" + apiKey, String.class);
     }
 }
